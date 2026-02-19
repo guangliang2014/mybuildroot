09fc829bbe2029ebd74e01b3cc03f8b49d08ea8b
ch.gl <glkaoyan@126.com>
2026-02-19 14:15:51 +0800

    add pcie extend board

---
+++ pcie/package/edu-test/edu-test.mk
@@ -0,0 +1,23 @@
+################################################################################
+# edu-test Buildroot package
+################################################################################
+
+EDU_TEST_VERSION = 1.0
+# Use local site (sources included in the external)
+EDU_TEST_SITE = $(BR2_EXTERNAL_PCIE_PATH)/package/edu-test
+EDU_TEST_SITE_METHOD = local
+EDU_TEST_SOURCE = test.c
+EDU_TEST_LICENSE = MIT
+
+EDU_TEST_DEPENDENCIES =
+
+define EDU_TEST_BUILD_CMDS
+	# compile from the package build directory (rsynced by Buildroot)
+	$(TARGET_CC) $(TARGET_CFLAGS) -o $(@D)/test-edu $(@D)/test.c $(TARGET_LDFLAGS)
+endef
+
+define EDU_TEST_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/test-edu $(TARGET_DIR)/usr/bin/test-edu
+endef
+
+$(eval $(generic-package))
